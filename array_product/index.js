faf18088ad2c8240daa8f136048e052148401a68
Hany Rabah <hany.rabah@dubizzle.com>
2019-12-08 09:35:43 +0400

    Array Product problem

---
+++ array_product/index.js
@@ -0,0 +1,14 @@
+// This problem was asked by Uber.
+
+// Given an array of integers, return a new array such that each element
+// at index i of the new array is the product of all the numbers in the original array except the one at i.
+
+// For example, if our input was [1, 2, 3, 4, 5], the expected output would be
+// [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+
+// I would like also to use different solutions approach so if you have more than one solution
+// let's list it down.
+
+function solution(a) {}
+
+module.exports = solution;
---
+++ array_product/test.js
@@ -0,0 +1,10 @@
+const solution = require('./index');
+
+test('solution is a function', () => {
+  expect(typeof solution).toEqual('function');
+});
+// Adding more tests will be great.
+
+test('Array products is working returning the right values', () => {
+  expect(solution([1, 2, 3, 4, 5])).toEqual([120, 60, 40, 30, 24]);
+});
