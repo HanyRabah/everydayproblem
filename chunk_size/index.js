0357123988dc810fdbd932bae26989ac572288f6
allyraza <saytoally@hotmail.com>
2019-12-09 12:50:09 +0400

    Add naive solution for array chunk

--- chunk_size/index.js
+++ chunk_size/index.js
@@ -8,6 +8,18 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  if (!Array.isArray(array) || !size) {
+    return [];
+  }
+
+  const result = [];
+
+  for (let i = 0; i < array.length; i += size) {
+    result.push(array.slice(i, i + size));
+  }
+
+  return result;
+}
 
 module.exports = chunk;
