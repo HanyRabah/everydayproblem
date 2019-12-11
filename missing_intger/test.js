d99b5a26543b2c8ee00c8e2a113b50aff060b403
Hany Rabah <hany.rabah@dubizzle.com>
2019-12-11 11:14:26 +0400

    removing folder

--- missing_intger/test.js
+++
@@ -1,9 +0,0 @@
-const MissingInteger = require('./index');
-
-test('MissingInteger function is defined', () => {
-  expect(MissingInteger).toBeDefined();
-});
-
-test('Calling MissingInteger with diff prints out the correct values', () => {
-  expect([1, 2, 4]).toEqual(3);
-});
