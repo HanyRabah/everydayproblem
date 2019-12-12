a841e7e8a01d4273157401206fc347471f60af11
allyraza <saytoally@hotmail.com>
2019-12-12 09:51:36 +0400

    Add more test cases

--- frog_jump/test.js
+++ frog_jump/test.js
@@ -1,5 +1,23 @@
-const jump = require('./index');
+const jump = require("./index");
 
-test('function jump exists', () => {
-  expect(typeof jump).toEqual('function');
+test("function jump exists", () => {
+  expect(typeof jump).toEqual("function");
+});
+
+test("returns valid result for", () => {
+  const x = 10;
+  const y = 85;
+  const d = 30;
+  const want = 3;
+
+  expect(jump(x, y, d)).toEqual(want);
+});
+
+test("returns result for invalid input", () => {
+  const x = 0;
+  const y = 0;
+  const d = 3;
+  const want = 0;
+
+  expect(jump(x, y, d)).toEqual(want);
 });
