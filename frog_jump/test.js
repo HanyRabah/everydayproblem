d05076efd2bda9445108d5b09b637f35353ea1bc
Hany Rabah <hany.rabah@dubizzle.com>
2019-12-15 11:13:24 +0400

    Adding solution for frog jump

--- frog_jump/test.js
+++ frog_jump/test.js
@@ -3,3 +3,7 @@ const jump = require('./index');
 test('function jump exists', () => {
   expect(typeof jump).toEqual('function');
 });
+
+test('Calling Jump with X=10 Y=85 D=30 should return 3', () => {
+  expect(jump(10, 85, 30)).toEqual(3);
+});
